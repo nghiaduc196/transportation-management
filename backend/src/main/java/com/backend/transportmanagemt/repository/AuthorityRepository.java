48c668ef36c0813e5513de1f4ec8f36c800886c7
DESKTOP-3GEREFE\NghiaDuc <nghiaduc196@gmail.com>
2021-06-09 21:22:10 +0700

    add backend

---
+++ backend/src/main/java/com/backend/transportmanagemt/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.backend.transportmanagemt.repository;
+
+import com.backend.transportmanagemt.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
