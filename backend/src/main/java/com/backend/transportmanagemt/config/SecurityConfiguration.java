fd0ce1c534ab0ecd5837001e011c829ed9d66f00
DESKTOP-3GEREFE\NghiaDuc <nghiaduc196@gmail.com>
2021-06-12 18:29:18 +0700

    fix

--- backend/src/main/java/com/backend/transportmanagemt/config/SecurityConfiguration.java
+++ backend/src/main/java/com/backend/transportmanagemt/config/SecurityConfiguration.java
@@ -74,6 +74,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
             .authorizeRequests()
             .antMatchers("/api/authenticate").permitAll()
+            .antMatchers("/api/account").permitAll()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/account/reset-password/init").permitAll()
