0632d05576efa324de3bf1ae233085ee3c189a34
Wind <nghiancse04410@gmail.com>
2021-06-14 22:52:13 +0700

    add message login

--- frontend/src/app/views/login/login.component.ts
+++ frontend/src/app/views/login/login.component.ts
@@ -3,12 +3,15 @@ import {FormBuilder, Validators} from '@angular/forms';
 import {LoginService} from '../../core/services/login.service';
 import {Router} from '@angular/router';
 import {StateStorageService} from '../../core/services/state-storage.service';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: 'login.component.html'
+  templateUrl: 'login.component.html',
+  providers: [MessageService]
+
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
   loginForm = this.fb.group({
     username: ['', Validators.required],
     password: ['', Validators.required]
@@ -18,7 +21,8 @@ export class LoginComponent implements OnInit{
   constructor(private fb: FormBuilder,
               private loginService: LoginService,
               private router: Router,
-              private stateStorageService: StateStorageService,) {
+              private stateStorageService: StateStorageService,
+              private messageService: MessageService) {
   }
 
   ngOnInit(): void {
@@ -32,12 +36,14 @@ export class LoginComponent implements OnInit{
           this.authenticationError = false;
           this.router.navigate(['']);
           const redirect = this.stateStorageService.getUrl();
+          this.messageService.add({severity: 'success', summary: 'Thành công', detail: 'Đăng nhập thành công'});
           if (redirect) {
             this.stateStorageService.storeUrl(null);
             this.router.navigate([redirect]);
           }
         })
         .catch(() => {
+          this.messageService.add({severity: 'error', summary: 'Lỗi', detail: 'Tài khoản không đúng. Vui lòng nhập lại'});
           this.loginForm.reset();
           // if (window.innerWidth <= phoneSize) {
           this.authenticationError = true;
